29ff20161dcb954af22d2ca6e3b49349dc0113fc
Dmitrii Borisov <dm1.br@yandex.com>
2024-09-08 13:27:41 +0300

    feat: New tmux configuration

--- .config/update.sh
+++ .config/update.sh
@@ -2,3 +2,8 @@ echo 'Removing current nvim directory...'
 rm -rf ./nvim
 echo 'Copyin nvim directory from ~/.config/nvim...'
 cp -r ~/.config/nvim nvim
+
+echo 'Removing current tmux directory...'
+rm -rf ./tmux
+echo 'Copyin tmux directory from ~/.config/tmux...'
+cp -r ~/.config/tmux tmux
