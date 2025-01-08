3d6dc654bfcfe3df43178a303e24d71ba240d16a
Dmitrii Borisov <dm1.br@yandex.com>
2025-01-08 15:16:52 +0300

    feat (big feature): added vertico, orderless and consult packages

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -1,6 +1,3 @@
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
 (desktop-save-mode 1)
 (column-number-mode 1)
 (show-paren-mode 1)
@@ -33,6 +30,101 @@
   (setq doom-themes-enable-bold t)
   (load-theme 'doom-one t)
   (doom-themes-org-config))
+
+;;; Runtime optimizations
+;; PERF: A second, case-insensitive pass over `auto-mode-alist' is time wasted.
+(setq auto-mode-case-fold nil)
+
+;; PERF: Disable bidirectional text scanning for a modest performance boost.
+;;   I've set this to `nil' in the past, but the `bidi-display-reordering's docs
+;;   say that is an undefined state and suggest this to be just as good:
+(setq-default bidi-display-reordering 'left-to-right
+              bidi-paragraph-direction 'left-to-right)
+
+;; PERF: Disabling BPA makes redisplay faster, but might produce incorrect
+;;   reordering of bidirectional text with embedded parentheses (and other
+;;   bracket characters whose 'paired-bracket' Unicode property is non-nil).
+(setq bidi-inhibit-bpa t)  ; Emacs 27+ only
+
+;; Reduce rendering/line scan work for Emacs by not rendering cursors or regions
+;; in non-focused windows.
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+
+;; More performant rapid scrolling over unfontified regions. May cause brief
+;; spells of inaccurate syntax highlighting right after scrolling, which should
+;; quickly self-correct.
+(setq fast-but-imprecise-scrolling t)
+
+;; Font compacting can be terribly expensive, especially for rendering icon
+;; fonts on Windows. Whether disabling it has a notable affect on Linux and Mac
+;; hasn't been determined, but do it anyway, just in case. This increases memory
+;; usage, however!
+(setq inhibit-compacting-font-caches t)
+
+;; PGTK builds only: there's a timeout that adds latency to frame operations,
+;; like `make-frame-invisible', which Emacs frequently calls without a guard
+;; because it's inexpensive in non-PGTK builds. Lowering the timeout from the
+;; default 0.1 should make childframes and packages that manipulate them (like
+;; `lsp-ui', `company-box', and `posframe') feel much snappier. See
+;; emacs-lsp/lsp-ui#613.
+(when (boundp 'pgtk-wait-for-event-timeout)
+  (setq pgtk-wait-for-event-timeout 0.001))
+
+;; Increase how much is read from processes in a single chunk (default is 4kb).
+;; This is further increased elsewhere, where needed (like our LSP module).
+(setq read-process-output-max (* 64 1024))  ; 64kb
+
+;; Introduced in Emacs HEAD (b2f8c9f), this inhibits fontification while
+;; receiving input, which should help a little with scrolling performance.
+(setq redisplay-skip-fontification-on-input t)
+
+;; Performance on Windows is considerably worse than elsewhere. We'll need
+;; everything we can get.
+(when (boundp 'w32-get-true-file-attributes)
+  (setq w32-get-true-file-attributes nil    ; decrease file IO workload
+        w32-pipe-read-delay 0               ; faster IPC
+        w32-pipe-buffer-size (* 64 1024)))  ; read more at a time (was 4K)
+
+;; The GC introduces annoying pauses and stuttering into our Emacs experience,
+;; so we use `gcmh' to stave off the GC while we're using Emacs, and provoke it
+;; when it's idle. However, if the idle delay is too long, we run the risk of
+;; runaway memory usage in busy sessions. And if it's too low, then we may as
+;; well not be using gcmh at all.
+(setq gcmh-idle-delay 'auto  ; default is 15s
+      gcmh-auto-idle-delay-factor 10
+      gcmh-high-cons-threshold (* 16 1024 1024))  ; 16mb
+
+  ;;; Disable UI elements early
+;; PERF,UI: Doom strives to be keyboard-centric, so I consider these UI elements
+;;   clutter. Initializing them also costs a morsel of startup time. What's
+;;   more, the menu bar exposes functionality that Doom doesn't endorse or
+;;   police. Perhaps one day Doom will support these, but today is not that day.
+;;   By disabling them early, we save Emacs some time.
+
+;; HACK: I intentionally avoid calling `menu-bar-mode', `tool-bar-mode', and
+;;   `scroll-bar-mode' because their manipulation of frame parameters can
+;;   trigger/queue a superfluous (and expensive, depending on the window system)
+;;   frame redraw at startup. The variables must be set to `nil' as well so
+;;   users don't have to call the functions twice to re-enable them.
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+  ;;; Encodings
+;; Contrary to what many Emacs users have in their configs, you don't need more
+;; than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+;; ...but `set-language-environment' also sets `default-input-method', which is
+;; a step too opinionated.
+(setq default-input-method nil)
+;; ...And the clipboard on Windows is often a wider encoding (UTF-16), so leave
+;; Emacs to its own devices there.
+(unless (or 'cygwin 'windows-nt 'ms-dos)
+  (setq selection-coding-system 'utf-8))
 
 ;; Using garbage magic hack.
 (use-package gcmh
@@ -82,17 +174,17 @@
 (use-package evil-collection
   :after evil
   :config
-  (setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (setq evil-collection-mode-list '(dashboard dired ibuffer magit))
   (evil-collection-init))
 
-(ido-mode 1)
-(ido-everywhere 1)
-(setq ido-separator "\n")
+;; (ido-mode 1)
+;; (ido-everywhere 1)
+;; (setq ido-separator "\n")
 
 (use-package ivy
+  :disabled t
   :ensure t
   :demand t
-  :init
   :config
   (setq enable-recursive-minibuffers t)
   (setq ivy-use-virtual-buffers t)
@@ -101,15 +193,50 @@
   (ivy-prescient-mode +1))
 
 (use-package counsel
+  :disabled t
   :ensure t
   :demand t
-  :init
   :config (counsel-mode +1))
+
+(use-package vertico
+  :ensure t
+  :custom;
+  (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-count 8) ;; Show more candidates
+  (vertico-resize nil) ;; Grow and shrink the Vertico minibuffer
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+(use-package orderless
+  :ensure t
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package consult
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  :init
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  :config
+  (consult-customize
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file
+   consult--source-recent-file consult--source-project-recent-file consult--source-bookmark
+   :preview-key "C-SPC"))
 
 (use-package which-key
   :ensure t
   :demand t
-  :init
   :config
   (which-key-mode +1))
 
@@ -117,13 +244,11 @@
 (setq which-key-idle-secondary-delay 0)
 
 (use-package projectile
+  :disabled t
   :ensure t
   :demand t
-  :init
   :config
   (projectile-mode +1))
-
-(setq projectile-completion-system 'ivy)
 
 (use-package smartparens
   :ensure smartparens  ;; install the package
@@ -145,8 +270,6 @@
 
 (use-package markdown-mode)
   (use-package json-mode)
-
-
 
   (use-package typescript-mode
     :config
@@ -192,14 +315,13 @@
 
 (use-package flymake
   :ensure t
-  :init
   :config (flymake-mode t)
   :hook (emacs-lisp-mode typesript-mode))
 
 (use-package lsp-mode
+  :disabled t
   :ensure t
   :defer t
-  :demand t
   :init
   (setq lsp-keymap-prefix "C-c l")
   (setq lsp-enable-on-type-formatting nil)
@@ -228,6 +350,7 @@
   :commands lsp)
 
 (use-package lsp-ui
+  :disabled t
   :ensure t
   :defer t
   :init
@@ -240,25 +363,6 @@
   (setq lsp-ui-sideline-show-code-actions nil)
   :commands lsp-ui-mode)
 
-;; Insert new line below current line
-(global-set-key (kbd "<C-return>") (lambda ()
-                                     (interactive)
-                                     (end-of-line)
-                                     (newline-and-indent)))
-
-;; Insert new line above current line
-(global-set-key (kbd "<C-S-return>") (lambda ()
-                                       (interactive)
-                                       (previous-line)
-                                       (end-of-line)
-                                       (newline-and-indent)))
-
-;; Duplicate line and move to next
-(global-set-key (kbd "C-,") (lambda ()
-                              (interactive)
-                              (duplicate-line)
-                              (next-line)))
-
 (evil-ex-define-cmd "q" 'kill-current-buffer)
 (evil-ex-define-cmd "wq" (lambda () 
 			   (interactive)
@@ -267,8 +371,7 @@
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package general
-  :ensure t)
+(use-package general :ensure t)
 
 (general-create-definer leader-def
   :states '(normal insert motion visual emacs)
@@ -277,8 +380,7 @@
   :non-normal-prefix "M-SPC")
 
 (leader-def
-  ;; Improved M-x with counsel
-  "SPC" '(counsel-M-x :which-key "M-x")
+  "SPC" '(execute-extended-command :which-key "M-x")
   "w s m" '(which-key-show-major-mode :which-key "[W]K [M]AJOR MODE KEYMAPS"))
 
 (general-define-key
@@ -287,9 +389,16 @@
  "$" 'evil-last-non-blank
  "-" 'evil-last-non-blank)
 
+(general-define-key
+ :keymaps 'global
+ "C-," (lambda ()
+         (interactive)
+         (duplicate-line)
+         (next-line)))
+
 (leader-def
   "b" '(:ignore t :which-key "[B]uffer")
-  "b s" '(switch-to-buffer :which-key "[S]witch to buffer")
+  "b s" '(consult-buffer :which-key "[S]witch to buffer")
   "b p" '(previous-buffer :which-key "[P]revious buffer")
   "b n" '(next-buffer :which-key "[N]ext buffer"))
 
@@ -330,7 +439,8 @@
   :keymaps 'emacs-lisp-mode-map
   "m" '(:ignore t :which-key "[M]AJOR MODE KEYMAPS")
   "m e" '(:ignore t :which-key "[E]val")
-  "m e e" '(eval-last-sexp :which-key "[E]val [e]xpression"))
+  "m e e" '(eval-last-sexp :which-key "[E]val [e]xpression")
+  "m e r" '(eval-region :which-key "[E]val [r]region"))
 
 (leader-def
   :keymaps 'tuareg-mode-map
@@ -355,25 +465,23 @@
 
 (leader-def
   "f" '(:ignore t :which-key "[F]ile")
-  "f f" '(counsel-find-file :which-key "[F]ind file")
-  "f g" '(counsel-git :which-key "counsel [g]it")
+  "f f" '(find-file :which-key "[F]ind file")
   "f s" '(save-buffer :which-key "[S]ave file"))
 
 (leader-def
   "p" '(:ignore t :which-key "[P]roject")
-  "p f" '(projectile-find-file :which-key "[F]ind file in project")
-  "p d" '(projectile-find-dir :which-key "find [d]ir in project")
-  "p s" '(counsel-git-grep :which-key "[S]earch for occurencies"))
+  "p f" '(project-find-file :which-key "[F]ind file in project")
+  "p d" '(project-find-dir :which-key "find [d]ir in project")
+  "p s" '(consult-git-grep :which-key "[S]earch for occurencies")
+  "p b" '(:ignore t :which-key "[B]uffers")
+  "p b s" '(consult-project-buffer :which-key "[S]witch to buffer"))
 
 (leader-def
   "g" '(:ignore t :which-key "[G]it")
   "g s" '(magit-status  :which-key "magit [s]tatus"))
 
 (use-package move-text
-  :ensure t
-  :demand t
-  :init
-  :config)
+  :ensure t)
 
 (general-define-key
  :states '(motion normal visual)
--- .config/emacs/dm.el
+++ .config/emacs/dm.el
@@ -9,56 +9,52 @@
 
 ;;; Code:
 
-(defun modules-to-string (modules-list) (mapconcat (lambda (x) (symbol-name x)) modules-list " "))
+(setq modules-hash-table (make-hash-table :test 'eq))
+
+(defmacro dm/use-package! (name &rest args)
+  `(puthash ',name '(,@args) modules-hash-table))
 
 (defmacro dm/modules! (&rest modules)
-  "Modules string"
-  ;; (let ((ivy (plist-get modules ivy))) ivy)
-  ;; (split-string (modules-to-string modules) ":")
-  ;; modules-string
-  `(split-string (modules-to-string '(list ,@modules)) ":"))
+  "Modules loading macro"
+  `(let ((modules-symbols (car (cl-remove-if #'keywordp '(,@modules)))))
+	 (use-package '(symbol-value 'modules-symbols))
+	 ;;	 (dolist (package-key modules-symbols nil)
+	 ;;	   (use-package package-key))
+	 ;;		 (gethash package-key modules-hash-table)))
+     ))
 
+(org-babel-load-file "~/.config/emacs/config.org")
 
-(dm/modules!
- :completion
- ivy
- ido
+;; (dm/modules! :completion ivy)
 
- :tools
- magit)
+;; (dm/modules!
+;;  :completion
+;;  ivy
+;;  ido
+;;  ;; vertico
 
+;;  :tools
+;;  ;; lsp
+;;  magit
 
+;;  :ui
+;;  mood-line
 
-(dm/modules!
+;;  :lang
+;;  emacs-lisp
+;;  (javascript :formatter prettier :lsp nil)
+;;  web
+;;  org
+;;  ocaml 
+;;  json
+;;  markdown
+;;  sh
 
- :completion
- ivy
- ido
- ;; vertico
+;;  :checkers
+;;  flymake
 
- :tools
- ;; lsp
- magit
-
- :ui
- mood-line
-
- :lang
- emacs-lisp
- (javascript :formatter prettier :lsp nil)
- web
- org
- ocaml 
- json
- markdown
- sh
-
- :checkers
- flymake
-
- :config
- (default +bindings +smartparens))
-
+;;  :config
+;;  (default +bindings +smartparens))
 
 (provide 'dm)
 ;;; dm.el ends here
